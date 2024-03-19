75d3a4220ea9ef47abd9d9d34173f76e991e6a39
KingoBoiii <kingodk@outlook.dk>
2024-03-19 17:11:32 +0100

    Making base provider a disposable

--- VictorKrogh.Data/Providers/ProviderBase.cs
+++ VictorKrogh.Data/Providers/ProviderBase.cs
@@ -2,7 +2,7 @@
 
 namespace VictorKrogh.Data.Providers;
 
-public abstract class ProviderBase(IsolationLevel isolationLevel) : IProvider
+public abstract class ProviderBase(IsolationLevel isolationLevel) : Disposable, IProvider
 {
     public IsolationLevel IsolationLevel => isolationLevel;
 
