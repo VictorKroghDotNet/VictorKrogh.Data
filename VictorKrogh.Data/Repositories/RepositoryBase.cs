c885be11cab745ba40e5266ba7783ac6e9353a13
KingoBoiii <kingodk@outlook.dk>
2024-03-19 16:51:05 +0100

    Added base implementation for IRepository

---
+++ VictorKrogh.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,9 @@
+﻿using VictorKrogh.Data.Providers;
+
+namespace VictorKrogh.Data.Repositories;
+
+public abstract class RepositoryBase<TProvider>(TProvider provider) : IRepository
+    where TProvider : IProvider
+{
+    protected TProvider Provider => provider;
+}
