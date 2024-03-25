ade90ef2c81c65f17a1b820c1453e0d0b720ec5e
KingoBoiii <kingodk@outlook.dk>
2024-03-25 10:06:25 +0100

    Added Unit of Work

---
+++ VictorKrogh.Data/UnitOfWork.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Data;
+using VictorKrogh.Data.Providers;
+using VictorKrogh.Data.Repositories;
+
+namespace VictorKrogh.Data;
+
+public interface IUnitOfWork : IProviderFactory, IDisposable
+{
+    IsolationLevel IsolationLevel { get; }
+    bool IsCompleted { get; }
+    void Commit();
+    TRepository GetRepository<TRepository>() where TRepository : IRepository;
+}
+
+internal sealed class UnitOfWork(IServiceProvider serviceProvider, IsolationLevel isolationLevel = IsolationLevel.ReadCommitted) : Disposable, IUnitOfWork
+{
+    public IsolationLevel IsolationLevel => isolationLevel;
+    public bool IsCompleted { get; set; }
+
+    private IList<IProvider> Providers { get; set; } = new List<IProvider>();
+
+    public void Commit()
+    {
+        foreach (var provider in Providers)
+        {
+            provider.Commit();
+        }
+
+        IsCompleted = true;
+    }
+
+    public TProvider CreateProvider<TProvider>(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted) where TProvider : IProvider
+    {
+        var providerFactory = serviceProvider.GetService<IProviderFactory<TProvider>>();
+        if (providerFactory is null)
+        {
+            throw new Exception($"No provider factory found for {typeof(TProvider).Name}");
+        }
+
+        var provider = providerFactory.CreateProvider(isolationLevel);
+
+        Providers.Add(provider);
+
+        return provider;
+    }
+
+    public TRepository GetRepository<TRepository>() where TRepository : IRepository
+    {
+        return serviceProvider.GetRequiredService<TRepository>();
+    }
+
+    protected override void DisposeManagedState()
+    {
+        Providers.Clear();
+    }
+}
