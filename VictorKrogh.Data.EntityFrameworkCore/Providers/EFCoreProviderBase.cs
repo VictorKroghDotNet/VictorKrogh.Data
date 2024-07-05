a594548a94ffee2a4c5ae2bfaf3f851968d98777
KingoBoiii <kingodk@outlook.dk>
2024-07-05 21:58:26 +0200

    Removed use of transactions

--- VictorKrogh.Data.EntityFrameworkCore/Providers/EFCoreProviderBase.cs
+++ VictorKrogh.Data.EntityFrameworkCore/Providers/EFCoreProviderBase.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
 using System.Data;
-using System.Data.Common;
 using VictorKrogh.Data.EntityFrameworkCore.Models;
 using VictorKrogh.Data.Providers;
 
@@ -14,31 +12,7 @@ public abstract class EFCoreDbProviderBase<TDbContext>(IsolationLevel isolationL
 
 public abstract class EFCoreProviderBase<TDbContext>(IsolationLevel isolationLevel, TDbContext dbContext) : ProviderBase(isolationLevel), IEFCoreProvider where TDbContext : DbContext
 {
-    private IDbTransaction? transaction;
-    private IDbConnection? connection;
-    private bool isCommitted = false;
-
     protected TDbContext Context => dbContext;
-
-    private IDbConnection Connection => connection ??= CreateConnection();
-
-    private IDbTransaction Transaction
-    {
-        get
-        {
-            if (transaction is not null)
-            {
-                return transaction;
-            }
-
-            if (Connection.State != ConnectionState.Open)
-            {
-                Connection.Open();
-            }
-
-            return transaction = Context.Database.BeginTransaction(IsolationLevel).GetDbTransaction();
-        }
-    }
 
     protected virtual IDbConnection CreateConnection()
     {
@@ -52,36 +26,26 @@ public abstract class EFCoreProviderBase<TDbContext>(IsolationLevel isolationLev
 
     public async ValueTask<IEnumerable<TModel?>> QueryAsync<TModel>(string sql, params object[] parameters) where TModel : EFCoreModel
     {
-        var ct = await Context.Database.UseTransactionAsync((DbTransaction)Transaction).ConfigureAwait(false);
-
         return await Context.Database.SqlQueryRaw<TModel>(sql, parameters).ToArrayAsync().ConfigureAwait(false);
     }
 
     public async ValueTask<TModel> QueryFirstAsync<TModel>(string sql, params object[] parameters) where TModel : EFCoreModel
     {
-        await Context.Database.UseTransactionAsync((DbTransaction)Transaction).ConfigureAwait(false);
-
         return await Context.Database.SqlQueryRaw<TModel>(sql, parameters).FirstAsync().ConfigureAwait(false);
     }
 
     public async ValueTask<TModel?> QueryFirstOrDefaultAsync<TModel>(string sql, params object[] parameters) where TModel : EFCoreModel
     {
-        await Context.Database.UseTransactionAsync((DbTransaction)Transaction).ConfigureAwait(false);
-
         return await Context.Database.SqlQueryRaw<TModel>(sql, parameters).FirstOrDefaultAsync().ConfigureAwait(false);
     }
 
     public async ValueTask<TModel> QuerySingleAsync<TModel>(string sql, params object[] parameters) where TModel : EFCoreModel
     {
-        await Context.Database.UseTransactionAsync((DbTransaction)Transaction).ConfigureAwait(false);
-
         return await Context.Database.SqlQueryRaw<TModel>(sql, parameters).SingleAsync().ConfigureAwait(false);
     }
 
     public async ValueTask<TModel?> QuerySingleOrDefaultAsync<TModel>(string sql, params object[] parameters) where TModel : EFCoreModel
     {
-        await Context.Database.UseTransactionAsync((DbTransaction)Transaction).ConfigureAwait(false);
-
         return await Context.Database.SqlQueryRaw<TModel>(sql, parameters).SingleOrDefaultAsync().ConfigureAwait(false);
     }
 
@@ -89,8 +53,6 @@ public abstract class EFCoreProviderBase<TDbContext>(IsolationLevel isolationLev
         where TModel : EFCoreModel
         where TKey : notnull
     {
-        await Context.Database.UseTransactionAsync((DbTransaction)Transaction).ConfigureAwait(false);
-
         return await Context.FindAsync<TModel>(key).ConfigureAwait(false);
     }
 
@@ -102,77 +64,35 @@ public abstract class EFCoreProviderBase<TDbContext>(IsolationLevel isolationLev
 
     public async ValueTask<bool> InsertAsync<TModel>(TModel model) where TModel : EFCoreModel
     {
-        await Context.Database.UseTransactionAsync((DbTransaction)Transaction).ConfigureAwait(false);
-
         return (await Context.AddAsync<TModel>(model).ConfigureAwait(false)).State == EntityState.Added;
     }
 
     public async ValueTask<bool> UpdateAsync<TModel>(TModel model) where TModel : EFCoreModel
     {
-        await Context.Database.UseTransactionAsync((DbTransaction)Transaction).ConfigureAwait(false);
-
         return Context.Update<TModel>(model).State == EntityState.Modified;
     }
 
     public async ValueTask<bool> DeleteAsync<TModel>(TModel model) where TModel : EFCoreModel
     {
-        await Context.Database.UseTransactionAsync((DbTransaction)Transaction).ConfigureAwait(false);
-
         return Context.Remove<TModel>(model).State == EntityState.Deleted;
     }
 
     public async ValueTask<int> ExecuteAsync(string sql, params object[] parameters)
     {
-        await Context.Database.UseTransactionAsync((DbTransaction)Transaction).ConfigureAwait(false);
-
         return await Context.Database.ExecuteSqlRawAsync(sql, parameters).ConfigureAwait(false);
     }
 
     public override void Commit()
     {
-        if (transaction == null)
-        {
-            return;
-        }
-
-        var ct = Context.Database.UseTransaction((DbTransaction)Transaction) ?? Context.Database.CurrentTransaction;
-
-        ct?.Commit();
-        isCommitted = true;
+        Context.SaveChanges();
     }
 
     public override void Rollback()
     {
-        if (transaction == null)
-        {
-            return;
-        }
-
-        var ct = Context.Database.UseTransaction((DbTransaction)Transaction) ?? Context.Database.CurrentTransaction;
-
-        ct?.Rollback();
     }
 
     protected override void DisposeManagedState()
     {
-        if (transaction != null)
-        {
-            if (!isCommitted)
-            {
-                Rollback();
-            }
-
-            transaction.Dispose();
-        }
-
-        if (connection != null)
-        {
-            if (connection.State != ConnectionState.Closed)
-            {
-                connection.Close();
-            }
-
-            connection.Dispose();
-        }
+        Context.Dispose();
     }
 }
